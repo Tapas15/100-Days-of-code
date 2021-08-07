99c3e7ac74f4c1565459d621f04217689b32f3f6
Tapas <tapasmahanandia@gmail.com>
2021-08-08 00:26:04 +0530

    day11

---
+++ Day-11/testing/main.py
@@ -0,0 +1,25 @@
+
+def my_function():
+  for i in range(1, 20):
+    i -= 1
+  
+  if i == 20:
+    print("You got it")
+
+my_function()
+
+a = '''
+Hello
+You
+'''
+
+print(a)
+
+
+# Reproduce the bug - Describe the Problem
+# Play computer
+# Use print
+# Use a debugger
+# Use the editor error Google the error - bugs with error and no error
+# Take a break & Ask a friend
+# Run often, solve 1 bug at a time
