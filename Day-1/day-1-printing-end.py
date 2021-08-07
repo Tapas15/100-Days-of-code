32aee684c4a84453d7319ac56e41f43dbd6d6954
Tapas <tapasmahanandia@gmail.com>
2021-08-07 13:11:44 +0530

    comit in printing end file

--- Day-1/day-1-printing-end.py
+++ Day-1/day-1-printing-end.py
@@ -1,1 +1,3 @@
-# Write your code below this line 👇
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+print("Hello " + input("What is your name?"))
